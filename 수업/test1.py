a3de3317e417ba29a1312850b1e8309bfb3b37d7
HeeSungLee <a73482006@gmail.com>
2022-09-26 21:36:22 +0900

    0926

---
+++ 수업/test1.py
@@ -0,0 +1,11 @@
+# # import main
+
+# # main.price(3)
+# # main.price_morning(4)
+# # main.price_soldier(5)
+
+# from test import * # 모듈 안의 모든 함수를 들고와서 여기서 선언한 것 처럼 만들어 줌
+# price(3)
+
+from test import price as pr
+pr(3242)
