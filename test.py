37ed31ae30458fb82cf9b94cf776acf3c7accd99
HeeSungLee <a73482006@gmail.com>
2022-11-18 22:36:07 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,4 @@
-# list
-a = [1, 1, 2, 3]
-
-# set
-b = {1, 1, 2, 3}
-
-# tuple
-c = (1, 1, 2, 3)
-
-# dictionary
-d = {"A": "a", "B": "b"}
-
-data = " a : {}\n b : {}\n c : {}\n d : {}"
-print(data.format(a, b, c, d["A"]))
-print()
-for key, value in d.items():
-    print(f"key : {key}, value : {value}")
+a = input('입력 : ')
+for i in range(1,a):
+    if i%2==0 :
+        print(i)
